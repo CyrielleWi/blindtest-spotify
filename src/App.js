f1f0e4bb1dcf00be476163215dc0ea115571e645
Cyrielle Willerval <cyrielle.willerval@ensta-paristech.fr>
2019-01-21 12:18:06 +0100

    step 4 and bonus1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 import Sound from 'react-sound';
 import Button from './Button';
 
-const apiToken = 'BQAkSeo8v3_A2N0dburZOiTuePAvY-qjDI6PLABwFj3dy2F6pD5BiLNWWYqtag0SsRytD1urZYBI0Uewnwqkoxzvo0l_PaDDwfRug1YrjU8txX7GNSGUrlcbabUgAIHk-sDJxNwxon2vLF1b_nc5e5oScYpn';
+const apiToken = 'BQDoYktmwzJL_plZKoJunuzD5Do10ZPemkE1QPKMS_LXwX7jIYhxX5TarVwQfVSCPL0xwBiQoqB8pfDMmoz4fOYn-FeFkVrdqxnN71kO0Xz1rHP6XMgHFuFfTJxl5CPv5qcKz1rQk1jssXmPyIeyXOHDkUTb';
 
 function shuffleArray(array) {
   let counter = array.length;
@@ -41,9 +41,46 @@ class App extends Component {
     super();
     this.state = {
       songsLoaded: false,
-      musicData: null
+      musicData: null,
+      musicNumber: 0,
+      currentTrack: null,
+      timeout: null
     };
     console.log("Constructor");
+  }
+
+  checkAnswer(answerId) {
+    if (answerId == this.state.currentTrack.id) {
+      swal('Bravo', "Bien ouej c'est la bonne musique!!", 'success')
+        .then(this.newTrack())
+        /*.then(clearTimeout(this.state.timeout))*/
+    }
+    else {
+      swal("C'est faux", "Quel échec", 'error').then(this.newTrack());
+    }
+  }
+
+  chooseTracks() {
+    let randomTrack1 = this.state.musicData[getRandomNumber(this.state.musicNumber)].track;
+    let randomTrack2 = this.state.musicData[getRandomNumber(this.state.musicNumber)].track;
+
+    while (randomTrack1.id === this.state.currentTrack.id || randomTrack1.id === randomTrack2.id || randomTrack2.id === this.state.currentTrack.id) {
+      randomTrack1 = this.state.musicData[getRandomNumber(this.state.musicNumber)].track;
+      randomTrack2 = this.state.musicData[getRandomNumber(this.state.musicNumber)].track;
+    }
+    
+    return [this.state.currentTrack, randomTrack1, randomTrack2];
+  }
+
+  newTrack() {
+    console.log("Début newTrack : ", this.currentTrack);
+    let newCurrentTrack = this.state.musicData[getRandomNumber(this.state.musicNumber)].track;
+    
+    this.setState({
+      currentTrack: newCurrentTrack
+    })
+
+    console.log("Fin newTrack : ", this.currentTrack);
   }
 
   componentDidMount() {
@@ -56,9 +93,13 @@ class App extends Component {
         .then(response => response.json())
         .then((data) => {
           console.log("Réponse reçue ! Voilà ce que j'ai reçu : ", data);
+          const randomNumber = getRandomNumber(data.items.length);
           this.setState({
             musicData: data.items,
             songsLoaded: true,
+            musicNumber: data.items.length,
+            currentTrack: data.items[randomNumber].track/*,
+            timeout: setTimeout(this.newTrack(), 30000)*/
         })
         })  
   }  
@@ -75,9 +116,13 @@ class App extends Component {
       }
 
       else {
-        const track1 = this.state.musicData[0].track;
-        const track2 = this.state.musicData[1].track;
-        const track3 = this.state.musicData[2].track;
+
+        let track1;
+        let track2;
+        let track3;
+
+        let musics = this.chooseTracks();
+        musics = shuffleArray(musics);
 
         return (
           <div className="App">
@@ -86,13 +131,13 @@ class App extends Component {
               <h1 className="App-title">Bienvenue sur le Blindtest</h1>
             </header>
             <div className="App-images">
-              <AlbumCover track={this.state.musicData[0].track}/>
+              <AlbumCover track={this.state.currentTrack}/>
             </div>
-            <Sound url={this.state.musicData[0].track.preview_url} playStatus={Sound.status.PLAYING}/>
-            <div className="App-buttons">
-              <Button>{track1.name}</Button>
-              <Button>{track2.name}</Button>
-              <Button>{track3.name}</Button>
+            <Sound url={this.state.currentTrack.preview_url} playStatus={Sound.status.PLAYING}/>
+            <div className="App-buttons"> 
+              {musics.map(item => 
+                <Button onClick={() => this.checkAnswer(item.id)}>{item.name}</Button>
+              )}
             </div>
           </div>
       );
